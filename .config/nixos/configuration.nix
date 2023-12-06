abf147052bbfb07e2f4e833cb000911ff6dc082b
agryphus <agryphus@gmail.com>
2023-12-05 22:55:36 -0500

    Remove boot loader vars in main config

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -21,10 +21,6 @@ in
       ./profiles/xdesktop.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
