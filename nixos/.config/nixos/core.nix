9fd2f2405ac473bc12f399170a8502180f57554f
agryphus <agryphus@gmail.com>
2024-03-18 16:42:32 -0400

    Add extra locale settings

--- nixos/.config/nixos/core.nix
+++ nixos/.config/nixos/core.nix
@@ -31,6 +31,17 @@ in {
   };
 
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
 
   fonts.packages = with pkgs; [
     # The nerdfonts package does not allow the installation of only the
